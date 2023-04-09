941f8c0bb9e48a5073d9b0539101abc3e290d2d6
othiagos <thg.santos@outlook.com>
2023-04-09 11:54:38 -0300

    Add Paser.cpp

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 class Parser {
 private:
-    
 public:
-    Parser() { }
-    ~Parser() { }
-    void read(std::string) { }
-    void infix() { }
-    void postfix() { }
-    void solve() { }
+    Parser();
+    ~Parser();
+
+    void read(std::string input);
+    void infix();
+    void postfix();
+    void solve();
 };
---
+++ src/Parser.cpp
@@ -0,0 +1,21 @@
+#include "Parser.hpp"
+
+Parser::Parser() { }
+
+Parser::~Parser() { }
+
+void Parser::read(std::string input) {
+    std::cout << input << std::endl;
+}
+
+void Parser::infix() {
+
+}
+
+void Parser::postfix() {
+
+}
+
+void Parser::solve() {
+
+}
