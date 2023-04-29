10dc4f723c812295c81e6d47238a1fcc50d07500
othiagos <clust3rr@gmail.com>
2023-04-29 14:49:04 -0300

    remove functions

--- include/Util.hpp
+++ include/Util.hpp
@@ -3,16 +3,9 @@
 #include <string>
 
 namespace util {
-    void remove_left_white_space(std::string &text);
-    void remove_right_white_space(std::string &text);
-    void rm_border_space(std::string &text);
-    void rm_bracket_border(std::string &text);
     bool is_empty_space(char alpha);
     bool is_bracket(char alpha);
-    bool round_bracket(char alpha);
     bool is_operator(char alpha);
     bool is_number(char alpha);
     void replace_char(std::string &str, std::string ex, std::string by);
-    double calc_operator(char alpha, double number1, double number2);
-    std::string get_number(std::string exp, int &i);
 }
--- src/Util.cpp
+++ src/Util.cpp
@@ -1,54 +1,11 @@
 #include "Util.hpp"
 
-void util::remove_left_white_space(std::string &text) {
-    while (is_empty_space(*text.begin()))
-        text.erase(text.begin());
-}
-
-void util::remove_right_white_space(std::string &text) {
-    while (is_empty_space(*(text.end() - 1)))
-        text.erase(text.end() - 1);
-}
-
-void util::rm_border_space(std::string &text) {
-    remove_left_white_space(text);
-    remove_right_white_space(text);
-}
-
-void util::rm_bracket_border(std::string &text) {
-    int start = 0, bracket_stage = 0;
-
-    for (int i = 0; i < (int) text.length(); i++) {
-        if (text[i] == '(') {
-            if (bracket_stage == 0)
-                start = i;
-            bracket_stage++;
-        }
-
-        if (text[i] == ')') {
-            bracket_stage--;
-            if (bracket_stage == 0) {
-                text.erase(text.begin() + i);
-                text.erase(text.begin() + start);
-            }
-        }
-    }
-}
-
 bool util::is_empty_space(char alpha) {
     return alpha == ' ' || alpha == '\t';
 }
 
 bool util::is_bracket(char alpha) {
     return alpha == '(' || alpha == ')';
-}
-
-bool util::round_bracket(char alpha) {
-    if (alpha == '(')
-        return true;
-    if (alpha == ')')
-        return false;
-    return false;
 }
 
 bool util::is_operator(char alpha) {
@@ -68,43 +25,4 @@ bool util::is_number(char alpha) {
 void util::replace_char(std::string &str, std::string ex, std::string by) {
     if (str.find(ex) != std::string::npos)
         str.replace(str.find(ex), 1, by);
-}
-
-double util::calc_operator(char alpha, double n1, double n2) {
-    switch (alpha) {
-    case '+':
-        return n1 + n2;
-        break;
-
-    case '-':
-        return n1 - n2;
-        break;
-
-    case '*':
-        return n1 * n2;
-        break;
-
-    case '/':
-        if (n2 != 0)
-            return n1 / n2;
-        throw "Invalid division";
-        break;
-    
-    default:
-        return 0;
-        break;
-    }
-}
-
-std::string util::get_number(std::string exp, int &i) {
-    for (int j = 0; util::is_number(exp[i + j]); j++) {
-
-        if (!util::is_number(exp[i + j + 1])) {
-            std::string number = exp.substr(i, j + 1);
-            i += j;
-            return number;
-        }
-
-    }
-    return "";
 }
